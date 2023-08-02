17a9482414629417184e6318814f916604bd5c4a
Luna Manandhar <luna.manandhar4@gmail.com>
2023-08-02 12:14:50 +0545

    added

--- src/components/Page3.tsx
+++ src/components/Page3.tsx
@@ -6,23 +6,30 @@ const Page3 = () => {
   return (
     <div className="page3-container">
       <img src="/src/assets/page3.svg" alt="Page 3 SVG" className="page3-svg" />
-      <img src="/src/assets/logo.png" className="logo" />
+      <div className="content"><img src="/src/assets/logo.png" className="logo" />
       <div className="box">
         <div className="counts">
           <div className="value">12</div>Days
-        </div><div className="coln">:</div>
+        </div>
+        <div className="coln">:</div>
         <div className="counts">
-        <div className="value">12</div>Hours
-        </div><div className="coln">:</div>
+          <div className="value">12</div>Hours
+        </div>
+        <div className="coln">:</div>
         <div className="counts">
-        <div className="value">12</div>Minutes
-        </div><div className="coln">:</div>
+          <div className="value">12</div>Minutes
+        </div>
+        <div className="coln">:</div>
         <div className="counts">
-        <div className="value">12</div>Seconds
+          <div className="value">12</div>Seconds
         </div>
       </div>
-      <div className="email">Notify me
-      <div className="iconCircle"><img className="iconSend" src="/src/assets/sendIcon.png"></img></div>
+      <div className="email">
+        Notify me
+        <div className="iconCircle">
+          <img className="iconSend" src="/src/assets/sendIcon.png"></img>
+        </div>
+      </div>
       </div>
     </div>
   );
